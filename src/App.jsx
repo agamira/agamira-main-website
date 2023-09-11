867e02f27d3da9fc82360b3d98a2bf3aee32e21b
Jeyhun Abilov <abilovv599@gmail.com>
2023-09-11 05:36:52 +0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Button } from "./components";
+
+function App() {
+  return (
+    <>
+      <Button className={"btn-primary"}>
+        Get Started
+      </Button>
+    </>
+  );
+}
+
+export default App;
