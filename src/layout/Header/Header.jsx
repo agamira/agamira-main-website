a9517042fb142b441267307e29a8695dd3134b28
Jeyhun Abilov <abilovv599@gmail.com>
2023-09-13 08:01:51 +0400

    add welcome section and fixes

--- src/layout/Header/Header.jsx
+++ src/layout/Header/Header.jsx
@@ -1,7 +1,33 @@
-import './Header.scss';
+import "./Header.scss";
+import burgerBtn from "../../assets/icons/burger-menu.svg";
+import logo from "../../assets/img/logo.png";
+import { Link } from "react-router-dom";
+import { Button } from "../../components";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header>
+      <div className="box">
+        <div className="logo">
+          <Link to={"/"}>
+            <img src={logo} alt="" />
+          </Link>
+        </div>
+        <nav>
+          <ul>
+            <li></li>
+          </ul>
+        </nav>
+        <div className="buttons">
+          <Link className="register-btn">Sign Up</Link>
+          <Button className="login-btn btn btn--outline">Log In</Button>
+          <button className="burger-menu">
+            <img src={burgerBtn} alt="" />
+          </button>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export { Header };
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,44 @@
-import { Button } from "../../components";
 import "./Home.scss";
+import { Button } from "../../components";
+import mira from "../../assets/img/mask-group-5.png";
+import logo from "../../assets/img/Polygon3.png";
 
 const Home = () => {
   return (
     <main>
-      <Button className={"btn btn--secondary"}>Learn More</Button>
+      <section className="welcome">
+        <div className="container">
+          <h1>
+            Welcome to AGAMIRA An artificial intelligence-powered platform
+          </h1>
+          <p>
+            Designed to help individuals to build successful businesses without
+            large investments
+          </p>
+          <div className="mira">
+            {/* <p>AGAMIRA</p> */}
+            <img className="logo" src={logo} alt="" />
+            <img className="mira-image" src={mira} alt="" />
+            <div className="buttons">
+              <Button className={"btn--outline btn--arrow-right"}>
+                Learn More
+              </Button>
+              <Button className={"btn btn--primary"}>Get Started</Button>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="mission">
+        <div className="container">
+          <h5>
+            Welcome to Agamira - The Future of Secure Digital Transactions
+          </h5>
+          <div className="box">
+            <div className="box--text"></div>
+            <div className="box--image"></div>
+          </div>
+        </div>
+      </section>
     </main>
   );
 };
