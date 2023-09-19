c0346d9406e3257b553cae574afffa0701c11d3e
Jeyhun Abilov <abilovv599@gmail.com>
2023-09-19 15:16:49 +0400

    some changes

--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,9 @@
-export { Button } from "./Button/Button.jsx";
+export { Button } from "./Button/Button";
+export { Logo } from "./Logo/Logo";
 export { Card } from "./Card/Card.jsx";
-export { SocialIcon } from "./SocialIcon/SocialIcon.jsx";
-export { CardIcon } from "./CardIcon/CardIcon.jsx";
-export { OrderedList } from "./OrderedList/OrderedList.jsx";
-export { Image } from "./Image/Image.jsx";
-export { SubscribeButton } from "./SubscribeButton/SubscribeButton.jsx";
-export { SubscriptionForm } from "./SubscriptionForm/SubscriptionForm.jsx";
+export { SocialIcon } from "./SocialIcon/SocialIcon";
+export { CardIcon } from "./CardIcon/CardIcon";
+export { OrderedList } from "./OrderedList/OrderedList";
+export { Image } from "./Image/Image";
+export { SubscribeButton } from "./SubscribeButton/SubscribeButton";
+export { SubscriptionForm } from "./SubscriptionForm/SubscriptionForm";
