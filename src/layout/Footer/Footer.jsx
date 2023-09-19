6abc36add366aed312e7d6d4dd08fdbfce9f374e
Jeyhun Abilov <abilovv599@gmail.com>
2023-09-19 19:02:27 +0400

    add Footer

--- src/layout/Footer/Footer.jsx
+++ src/layout/Footer/Footer.jsx
@@ -1,7 +1,39 @@
-import './Footer.scss';
+import { Link } from "react-router-dom";
+import { Logo, SubscriptionFormBig } from "../../components";
+import "./Footer.scss";
 
 const Footer = () => {
-  return <footer>Footer</footer>;
+  return (
+    <footer>
+      <div className="container">
+        <div className="box">
+          <div className="box__form">
+            <Logo />
+            <ul>
+              <li>
+                <Link>About Us</Link>
+              </li>
+              <li>
+                <Link>Careers</Link>
+              </li>
+              <li>
+                <Link>Blog</Link>
+              </li>
+              <li>
+                <Link>Pricing</Link>
+              </li>
+            </ul>
+            <SubscriptionFormBig />
+          </div>
+        </div>
+      </div>
+      <div className="copyright">
+        <p>
+          Copyright © Product | Designed by <span>Agamira LTD</span>
+        </p>
+      </div>
+    </footer>
+  );
 };
 
 export { Footer };
