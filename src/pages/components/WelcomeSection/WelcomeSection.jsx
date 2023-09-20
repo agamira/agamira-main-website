f9169f9812feea5133fcf9f76caf7b346048e5ea
Jeyhun Abilov <abilovv599@gmail.com>
2023-09-20 16:01:28 +0400

    added tablet media queries for welcome section

--- src/pages/components/WelcomeSection/WelcomeSection.jsx
+++ src/pages/components/WelcomeSection/WelcomeSection.jsx
@@ -7,11 +7,21 @@ const WelcomeSection = () => {
   return (
     <section className="welcome">
       <div className="container">
-        <h1>Welcome to AGAMIRA An artificial intelligence-powered platform</h1>
-        <p>
-          Designed to help individuals to build successful businesses without
-          large investments
-        </p>
+        <div className="section-title">
+          <h1>
+            Welcome to AGAMIRA An artificial intelligence-powered platform
+          </h1>
+          <p>
+            Designed to help individuals to build successful businesses without
+            large investments
+          </p>
+          <div className="buttons">
+            <Button className={"btn--outline btn--arrow-right"}>
+              Learn More
+            </Button>
+            <Button className={"btn btn--primary"}>Get Started</Button>
+          </div>
+        </div>
         <div className="mira">
           <div className="box">
             <Image source={logo} className={"logo"} alt={"logo"} />
