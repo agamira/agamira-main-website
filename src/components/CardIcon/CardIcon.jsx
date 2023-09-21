292a180b75b3e63fa2c6a7c5b8b7dd220734a907
Jeyhun Abilov <abilovv599@gmail.com>
2023-09-21 15:18:42 +0400

    add alt attribute

--- src/components/CardIcon/CardIcon.jsx
+++ src/components/CardIcon/CardIcon.jsx
@@ -3,7 +3,7 @@ import "./CardIcon.scss";
 const CardIcon = ({ className = "", logo }) => {
   return (
     <div className={`card-icon ${className}`}>
-      {!!logo && <img className="card-icon__logo" src={logo} alt="" />}
+      {!!logo && <img className="card-icon__logo" src={logo} alt="icon" />}
     </div>
   );
 };
